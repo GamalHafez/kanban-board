ac560627cc9ffa7b5d647e6df7edb3dca4068ab6
GamalHafez <gamal.k.hafez@gmail.com>
2026-07-08 18:22:39 +0300

    feat(server): implement a logger middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,11 +8,13 @@ import authRoutes from "@routes/auth.routes.js";
 // Import middlewares
 import errorMiddleware from "@middlewares/errorMiddleware.js";
 import routeNotFound from "@middlewares/routeNotFound.js";
+import logger from "@middlewares/logger.js";
 
 const app = express();
 
 // Global middlewares
 app.use(cors());
+app.use(logger);
 app.use(express.json());
 
 // Routes
