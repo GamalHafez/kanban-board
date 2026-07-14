ccb4a43e1b859a7b071c5ec4b6a6a59509b25849
GamalHafez <gamal.k.hafez@gmail.com>
2026-07-14 10:42:12 +0300

    feat(server): implement authenticated get boards endpoint

--- server/src/controllers/boards.controller.ts
+++ server/src/controllers/boards.controller.ts
@@ -1,8 +1,23 @@
 import { NextFunction, Request, Response } from "express";
+import { prisma } from "@config/db.js";
 
-export const getBoards = (req: Request, res: Response, next: NextFunction) => {
+export const getBoards = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   try {
-    res.json({ mess: "teeeeest" });
+    const boards = await prisma.board.findMany({
+      where: { userId: req.user?.id },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    res.status(200).json({
+      success: true,
+      data: { boards },
+    });
   } catch (err) {
     next(err);
   }
