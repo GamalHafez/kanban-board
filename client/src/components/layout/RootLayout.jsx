9bf63c484842ae9c961e981d5177b75271a0ea16
GamalHafez <gamal.k.hafez@gmail.com>
2026-07-20 15:59:35 +0300

    fix(client): adjust styles for Header and HomePage for better mobile responsiveness

--- client/src/components/layout/RootLayout.jsx
+++ client/src/components/layout/RootLayout.jsx
@@ -12,7 +12,7 @@ const RootLayout = () => {
 
   return (
     <main className="font-jakarta flex h-screen flex-col">
-      <header className="text-main-blue border-lines-light flex h-[65px] shrink-0 items-center gap-10 border-b bg-white capitalize md:h-[97px] lg:h-[97px]">
+      <header className="text-main-blue border-lines-light flex h-[65px] shrink-0 items-center justify-between gap-10 border-b bg-white px-6 capitalize md:h-[97px] lg:h-[97px]">
         <HeaderIdentity />
         <div className="flex w-full items-center justify-between">
           {isBoardPage && (
